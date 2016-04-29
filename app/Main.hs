ed72d2405aa0c81e60502de2585a3cdb412b2122
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-29 04:46:31 -0400

    Initial pass at adding color to output

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,45 @@
 module Main where
 
+import System.Console.ANSI
+import Data.List (nub)
 import Unused.TermSearch (search)
 import Unused.Parser (parseLines)
+import Unused.Types
 
 main :: IO ()
 main = do
     terms <- pure . lines =<< getContents
     results <- pure . concat =<< mapM search terms
-    let matches = parseLines $ unlines results
+    let groupedMatches = groupBy term $ parseLines $ unlines results
 
-    print matches
+    mapM_ printMatchPair groupedMatches
+
+    return ()
+
+printMatchPair :: (String, [TermMatch]) -> IO ()
+printMatchPair (term', matches) = do
+    setSGR   [SetColor Foreground Vivid Red]
+    setSGR   [SetConsoleIntensity BoldIntensity]
+    putStrLn term'
+    setSGR   [Reset]
+    printMatches matches
+    putStr "\n"
+
+printMatches :: [TermMatch] -> IO ()
+printMatches matches = do
+    mapM_ printMatch matches
+  where
+    printMatch m = do
+        setSGR [SetColor Foreground Dull Green]
+        putStr $ "  " ++ path m
+        setSGR [SetColor Foreground Dull Yellow]
+        putStr $ " " ++ (show . occurrences) m ++ " "
+        setSGR [Reset]
+        putStr "\n"
+
+groupBy :: Eq b => (a -> b) -> [a] -> [(b, [a])]
+groupBy f l =
+    fmap (\t -> (t, byTerm t)) uniqueTerms
+  where
+    byTerm t = filter (((==) t) . f) l
+    uniqueTerms = nub $ fmap f l
