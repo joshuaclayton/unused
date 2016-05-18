0268790d8353ee455fd2860bdc5edfaeab266adf
Joshua Clayton <joshua.clayton@gmail.com>
2016-05-17 23:15:27 -0400

    More perf improvements

--- src/Unused/Util.hs
+++ src/Unused/Util.hs
@@ -1,12 +1,23 @@
 module Unused.Util
     ( groupBy
+    , stringToInt
     ) where
 
 import Control.Arrow ((&&&))
 import qualified Data.List as L
 import Data.Function
+import Data.Char (digitToInt, isDigit)
 
 groupBy :: (Ord b) => (a -> b) -> [a] -> [(b, [a])]
 groupBy f = map (f . head &&& id)
                    . L.groupBy ((==) `on` f)
                    . L.sortBy (compare `on` f)
+
+stringToInt :: String -> Maybe Int
+stringToInt xs
+    | all isDigit xs = Just $ loop 0 xs
+    | otherwise = Nothing
+  where
+    loop acc [] = acc
+    loop acc (x:xs') = let acc' = acc * 10 + digitToInt x
+                       in loop acc' xs'
